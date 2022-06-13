98dd3aced9d6c6c129019b49d8df14b53235e89c
Lucas Escobar Faria Travesso <lucas.travesso@muralis.com.br>
2022-06-13 14:58:00 -0300

    formulario de cadastro

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,19 +1,28 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, children:[
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
       {
-        path: '', component: LoginComponent
-      }
-    ]
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'novousuario',
+        component: NovoUsuarioComponent,
+      },
+    ],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
---
+++ src/app/home/novo-usuario/novo-usuario.ts
@@ -0,0 +1,6 @@
+export interface NovoUsuario {
+  userName: String;
+  email: string;
+  fullName: string;
+  password: string;
+}
