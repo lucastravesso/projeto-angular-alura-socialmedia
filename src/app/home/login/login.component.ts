eb20b645aae8091c8c4f2de7e7610e6596564ef4
Lucas Escobar Faria Travesso <lucas.travesso@muralis.com.br>
2022-06-13 13:41:56 -0300

    criando componentes de de messageError

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,6 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario = '';
+  senha = '';
+
+  constructor(private authService: AutenticacaoService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.authService.autentica(this.usuario, this.senha).subscribe(() => {
+      this.router.navigate(['animais'])
+    }, (error) => console.log("Usuario ou senha invalidos")
+    )
   }
 
 }
